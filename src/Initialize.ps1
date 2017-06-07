afecf5132dca18cc702c7dfbd7d21a9cc4b750f9
Alex Novak <alexwnovak@gmail.com>
2017-06-06 21:19:55 -0400

    Now fleshes out a basic directory structure under Content

--- src/Initialize.ps1
+++ src/Initialize.ps1
@@ -6,6 +6,13 @@ function Initialize-Map
         [string] $MapName
     )
 
+    New-Directory "Content"
+    New-Directory "Content\maps"
+    New-Directory "Content\materials\$($MapName)"
+    New-Directory "Content\models"
+    New-Directory "Content\sounds"
+}
 
-    Write-Host "Map name: $($MapName)"
+function New-Directory( $path ) {
+    mkdir $path -Force > $null
 }
